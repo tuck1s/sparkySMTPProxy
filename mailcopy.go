466a4012b4eef27040d0eafdf444087abdb0319f
tuck1s <steve.tuck@sparkpost.com>
2019-09-02 17:11:41 +0100

    transfer using net/mail.ReadMessage for MIME types

--- mailcopy.go
+++ mailcopy.go
@@ -4,7 +4,9 @@ package main
 import (
 	"bufio"
 	"io"
-	"net/textproto"
+	"log"
+	"net/mail"
+	"strings"
 )
 
 /* If you just want to pass through the entire mail headers and body, you can just use
@@ -15,42 +17,39 @@ func MailCopy(dst io.Writer, src io.Reader) (int64, error) {
 }
 */
 
-const smtpCRLF = "\r\n"
-
-// MailCopy is called by the proxy to transfer the mail body from downstream (client) to upstream (server)
+// MailCopy transfers the mail body from downstream (client) to upstream (server)
 // The writer will be closed by the parent function, no need to close it here.
 func MailCopy(dst io.Writer, src io.Reader) (int64, error) {
 	var totalWritten int64
-	tp := textproto.NewReader(bufio.NewReader(src))
-	// Read mail headers, copying through to the destination
-	for {
-		l, err := tp.ReadLine()
-		if err != nil {
-			return totalWritten, err
+	const smtpCRLF = "\r\n"
+	message, err := mail.ReadMessage(bufio.NewReader(src))
+	if err != nil {
+		return totalWritten, err
+	}
+	// Pass through headers. The m.Header map does not preserve order, but that should not matter.
+	for hdrType, hdrList := range message.Header {
+		for _, hdrVal := range hdrList {
+			hdrLine := hdrType + ": " + hdrVal + smtpCRLF
+			log.Print("\t", hdrLine)
+			bytesWritten, err := dst.Write([]byte(hdrLine))
+			totalWritten += int64(bytesWritten)
+			if err != nil {
+				return totalWritten, err
+			}
 		}
-		// debug: log.Println("\t", l)
-		bytesWritten, err := dst.Write([]byte(l + smtpCRLF))
-		totalWritten += int64(bytesWritten)
-		if err != nil {
-			return totalWritten, err
-		}
-		if l == "" {
-			break // Blank line is the delimiter between headers and mail body
-		}
+	}
+	// Blank line denotes end of headers
+	bytesWritten, err := io.Copy(dst, strings.NewReader(smtpCRLF))
+	totalWritten += int64(bytesWritten)
+	if err != nil {
+		return totalWritten, err
 	}
 
-	// Read mail body. src is a textproto.DotReader and dst is a textproto.DotWriter
-	// which takes care of finding the end of the mail body, escaping dots and CRLF padding etc
-	for {
-		l, err := tp.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				err = nil // Normal exit
-			}
-			return totalWritten, err
-		}
-		// debug: log.Println("\t", l)
-		bytesWritten, err := dst.Write([]byte(l + smtpCRLF))
-		totalWritten += int64(bytesWritten)
+	// Copy the body
+	bytesWritten, err = io.Copy(dst, message.Body)
+	totalWritten += int64(bytesWritten)
+	if err != nil {
+		return totalWritten, err
 	}
+	return totalWritten, err
 }
